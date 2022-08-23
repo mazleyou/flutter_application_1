368a60c6584a474056308ab29f95601d3e22d294
mazleyou <mazle@nate.com>
2022-08-23 18:17:28 +0900

    chart menu add - 22-08-23

---
+++ lib/fl_chart/fl_chart.dart
@@ -0,0 +1,16 @@
+library fl_chart;
+
+export 'bar_chart/bar_chart.dart';
+export 'bar_chart/bar_chart_data.dart';
+export 'base/axis_chart/axis_chart_data.dart';
+export 'base/axis_chart/axis_chart_widgets.dart';
+export 'base/base_chart/base_chart_data.dart';
+export 'base/base_chart/fl_touch_event.dart';
+export 'line_chart/line_chart.dart';
+export 'line_chart/line_chart_data.dart';
+export 'pie_chart/pie_chart.dart';
+export 'pie_chart/pie_chart_data.dart';
+export 'radar_chart/radar_chart.dart';
+export 'radar_chart/radar_chart_data.dart';
+export 'scatter_chart/scatter_chart.dart';
+export 'scatter_chart/scatter_chart_data.dart';
---
+++ lib/main_backup.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'StartupNameGenerator.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Flutter Library',
+    initialRoute: '/',
+    routes: {
+      '/': (context) => const LibraryList(),
+      '/StartupNameGenerator': (context) => const StartupNameGenerator(),
+    },
+  ));
+}
+
+class _RandomWordsState extends State<LibraryList> {
+  final _biggerFont = const TextStyle(fontSize: 18);
+  final _itemList = <dynamic>[
+    ['Startup Name Generator', '/StartupNameGenerator'],
+    ['two', '/RouteTableMove'],
+    ['three', ''],
+    ['four', ''],
+    ['five', ''],
+    ['six', ''],
+    ['seven', ''],
+    ['eight', ''],
+    ['nine', ''],
+    ['ten', ''],
+    ['eleven', '']
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Library'),
+      ),
+      body: ListView.builder(
+          padding: const EdgeInsets.all(16.0),
+          itemCount: _itemList.length,
+          itemBuilder: (context, i) {
+            return ListTile(
+              title: Text(
+                _itemList[i][0],
+                style: _biggerFont,
+              ),
+              onTap: () {
+                Navigator.of(context).pushNamed(_itemList[i][1]);
+              },
+            );
+          }),
+    );
+  }
+}
+
+class LibraryList extends StatefulWidget {
+  const LibraryList({super.key});
+
+  @override
+  State<LibraryList> createState() => _RandomWordsState();
+}
